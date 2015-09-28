0dc1bc1cf2ce0c4505b7127a47b100328f0f950b
lyp <icidicf@gmail.com>
2015-09-28 09:35:53 -0700

    change ping to nping

--- bash/alias.sh
+++ bash/alias.sh
@@ -1,8 +1,24 @@
+#configure
+alias bashcfg='cd /home/lyp/local/bash'
+alias vimcfg='cd /home/lyp/local/vim'
+alias bin='cd /home/lyp/local/bin'
 alias sc='source ~/.bashrc'
-alias rs='sudo service tomcat7 restart'
-alias tm='cd /var/lib/tomcat7/webapps'
-TM=/var/lib/tomcat7/webapps
-alias chrome="chromium-browser"
+
+
+
+#system app
 alias install='sudo apt-get install'
 
-alias bashcfg='/home/lyp/local/bash'
+
+
+#application
+alias rs='sudo service tomcat7 restart'
+alias tm='cd /var/lib/tomcat7/webapps'
+alias chrome="chromium-browser"
+alias ping='nping'
+
+
+
+
+TM='/var/lib/tomcat7/webapps'
+UTILFUNC='/git/xunil/util_functions'
