73aa3f5737f501cafe06ce5faab2a403ff4b6634
lyp <icidicf@gmail.com>
2015-10-02 08:16:27 -0700

    add muduo C++ configure

---
+++ bash/project/muduo.sh
@@ -0,0 +1,4 @@
+MUDUO_HOME=/git/network/openProject/muduo
+
+export CPLUS_INCLUDE_PATH=$MUDUO_HOME/build/release-install/include/:$CPLUS_INCLUDE_PATH
+export LIBRARY_PATH=$MUDUO_HOME/build/release-install/lib:$LD_LIBRARY_PATH
