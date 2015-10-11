73952dc01e68c72bdb545385c0a5591f2d1bafe5
icidicf <icidicf@gmail.com>
2015-10-11 01:32:37 -0700

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,11 @@ then
     mkdir backup
 fi
 
+if [ ! -f ../.vimrc ]
+then 
+    touch ../.vimrc
+fi
+
 backupDir=./backup/
 
 now=$(date "+%Hh-%Mm-%Ss-%B-%d")
@@ -31,3 +36,13 @@ echo "source /home/lyp/local/bash/function.sh" >> $bashDestFile
 
 echo "source /home/lyp/local/vim/vimPlugingManager.vim" >> $vimDestFile
 echo "source /home/lyp/local/vim/basic.vim"   >> $vimDestFile
+
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+git config --global user.email "icidicf@gmail.com"
+git config --global user.name "icidicf"
+git config --global push.default simple
