5f57c7f06b29ce8d8b3e3ab46f78cd4356d3eda9
icidicf <icidicf@gmail.com>
2016-03-23 04:34:34 -0700

    fix LD_LIBRARY_PATH

--- bash/export.sh
+++ bash/export.sh
@@ -9,10 +9,15 @@ export PATH=~/local/bin/:$SCALA_HOME/bin:$MVN_HOME/bin:$PATH;
 export _JAVA_OPTIONS=-Xmx3072m
 
 #export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
 export J2EE_HOME=/usr/lib/jvm/glassfish4
 export CLASSPATH=./:$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$J2EE_HOME/glassfish/lyp_modules/*:/opt/eclipse/eclipse/plugins/*:/usr/local/lib/java_lib/struts-2.3.24/lib/*;
 
 
 #other part
 export ANT_OPTS=-Dfile.encoding=UTF-8
+
+LD_LIBRARY_PATH=/usr/local/lib
+export LD_LIBRARY_PATH
+ldconfig
+
